b3def4aa0e1594a771f361db1a6b293e9f66fd9a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-04 18:47:30 +0100

    add part2 of the test project and the mockObject files

---
+++ src/tests/part2/mockObject.js
@@ -0,0 +1,27 @@
+export const toDoMock = [
+  {
+    description: 'Walk the dog',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go to the gym',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Go the movie',
+    completed: true,
+    index: 3,
+  },
+  {
+    description: 'Write test',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Watch football',
+    completed: false,
+    index: 5,
+  },
+];
