4e8414a440a5853487cb89b8dcdf8bb74372fc2c
Rafael Echart <rafaelechartsaltos@gmail.com>
2021-11-03 21:53:07 -0500

    More tests for removetask

--- src/tests/Part1/removeTasks.test.js
+++ src/tests/Part1/removeTasks.test.js
@@ -22,6 +22,23 @@ describe('Remove only one element when call to function removeOnetask', () => {
   test('Remove one task when toDoList array length is 0 return undefined', () => {
     expect(removeOnetask(1)).toBe(undefined);
   });
+
+  test('Added 3 valid tasks, remove 1. Check the DOM for only two tasks painted in the DOM (each task element starts with the class="section")', () => {
+    global.document.getElementById('addNewInput').value = 'NewTask1';
+    addTaskToList();
+    global.document.getElementById('addNewInput').value = 'NewTask2';
+    addTaskToList();
+    global.document.getElementById('addNewInput').value = 'NewTask3';
+    addTaskToList();
+
+    removeOnetask(1)
+
+    const taskContainer = global.document.getElementById('tasks');
+    const taskCounter = taskContainer.getElementsByClassName('section');
+
+    expect(taskCounter.length).toBe(2);
+
+  });
 });
 
 describe('Remove all completed tasks when call to function removeCompleteTasksFromList', () => {
@@ -49,6 +66,29 @@ describe('Remove all completed tasks when call to function removeCompleteTasksFr
 
     expect(ToDoList.currentTasks.length).toBe(1);
     expect(window.localStorage.length).toBe(1);
+  });
+
+  test('Add 3 valid tasks, then set first 2 to completed === true, and delete all completed, Check the DOM for only two tasks painted in the DOM (each task element starts with the class="section")', () => {
+    global.document.getElementById('addNewInput').value = 'NewTask1';
+    addTaskToList();
+    global.document.getElementById('addNewInput').value = 'NewTask2';
+    addTaskToList();
+    global.document.getElementById('addNewInput').value = 'NewTask3';
+    addTaskToList();
+  
+
+    const toDoArray = ToDoList.currentTasks;
+    toDoArray[0].completed = true;
+    toDoArray[1].completed = true;
+
+    removeCompleteTasksFromList();
+
+    const taskContainer = global.document.getElementById('tasks');
+    const taskCounter = taskContainer.getElementsByClassName('section');
+
+
+    expect(taskCounter.length).toBe(1);
+
   });
 
   test('Remove all completed when toDoList array length is 0 return undefined', () => {
