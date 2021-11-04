1a470b3e7993ed8bca349ee194ec5731a35d2d4d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-11-04 18:49:42 +0100

    Add toDoMock tests

--- src/tests/part2/object.test.js
+++ src/tests/part2/object.test.js
@@ -1,3 +1,22 @@
 import { toDoMock } from "./mockObject";
 
 
+describe('toDoMock', () => {
+  test('return the length of the arrays of objects', () => {
+    
+    expect(toDoMock.length).toBe(5)
+  })
+
+  test('return the content of the toDoMock object', () => {
+    expect(toDoMock[0].description).toContain('Walk the dog')
+    
+  })
+  test('return the index of of the array', () => {
+    expect(toDoMock[0].index).toBe(1)
+    
+  })
+  test('return the of a task in the toDoMock array', () => {
+    expect(toDoMock[2].completed).toBe(true)
+    
+  })
+})
